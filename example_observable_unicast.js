a03c65cb7e7b6a69ce2820aaa45161ce473fdd90
Giuseppe Nesca <giuseppe.nesca@spindox.it>
2024-02-27 12:19:00 +0100

    node config first commit rp examples observable examples

---
+++ example_observable_unicast.js
@@ -0,0 +1,40 @@
+class Observable {
+    handlers = [];  // observer array
+
+    subscribe = (fn) => {
+        this.handlers.push(fn);
+        // console.log('subscribe', this.handlers.length)
+    }
+    unsubscribe = (fn) => {
+        this.handlers = this.handlers.filter(
+            (handler) => {
+                if (handler !== fn) {
+                    return handler;
+                }
+            }
+        );
+        // console.log('unsubscribe', this.handlers.length)
+    }
+    update = (value) => {
+        this.handlers.forEach((handler) => {
+            // handler.call(this, value) // multicast
+            handler.call(this, Math.random() * 10 + 1) // unicast
+        })
+    }
+}
+
+function run() {
+    var clickHandler = function (item) {
+        console.log("fired: " + item);
+    };
+    var subject = new Observable();
+    subject.subscribe((item) => console.log("uooooooo " + item))
+    subject.subscribe(clickHandler);
+    subject.update('event #1');
+    subject.unsubscribe(clickHandler);
+    subject.update('event #2');
+    subject.subscribe(clickHandler);
+    subject.update('event #3');
+}
+
+run()
